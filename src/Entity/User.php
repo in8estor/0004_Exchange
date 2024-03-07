4d99f2ecc6cbddbf77bdd31ff9ad00fa91c4b32a
asd2d <872122@mail.ru>
2024-03-08 00:13:51 +0300

    install doctrine and changed User.php

---
+++ src/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 12, unique: true, nullable: false)]
+    private string $phone;
+
+    #[ORM\Column(type: 'string', length: 50, unique: true, nullable: false)]
+    private string $email;
+
+    #[ORM\Column(type: 'string', length: 50, nullable: false)]
+    private string $name;
+
+    #[ORM\Column(type: 'string', length: 50, nullable: false)]
+    private string $password;
+
+    #[ORM\Column(type: 'datetime')]
+    private DateTimeInterface $createdAt;
+
+    #[ORM\Column(type: 'datetime')]
+    private DateTimeInterface $updatedAt;
+
+
+
+
+
+
+
+}
