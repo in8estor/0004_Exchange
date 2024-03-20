a91321eba6207df399dfad1e7b8c47882f54a328
asd2d <872122@mail.ru>
2024-03-20 23:40:59 +0300

    some changes in entity Product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -36,6 +36,13 @@ class Product
         return $this->id;
     }
 
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getCategory(): ?string
     {
         return $this->category;
