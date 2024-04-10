dd43d6131cfc8b0db739be33e3c2121605ef2be4
asd2d <872122@mail.ru>
2024-04-10 16:41:21 +0300

    add method save

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,7 +15,7 @@ class RegistrationController extends AbstractController
     #[Route('/registration', name: 'app_registration', methods: 'post')]
     public function create(Request $request, UserRepository $userRepository): JsonResponse
     {
-        $emailData = json_decode($request->getEmail(), true);
+        $emailData = json_decode($request->getContent(), true);
         $email = (new User())->setEmail($emailData['email']);
         $userRepository->save($email, true);
         
